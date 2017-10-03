67f9b7ba59af8a6d688f3ee5dfda8a62bb00e9dc
jeremy-davis-mcgill <jeremy.davis@mail.mcgill.ca>
2017-10-02 22:12:09 -0400

    Partial Commenting and Java Style

--- src/ca/mcgill/ecse211/navigationlab/Navigation.java
+++ src/ca/mcgill/ecse211/navigationlab/Navigation.java
@@ -1,268 +1,159 @@
 package ca.mcgill.ecse211.navigationlab;
 
-import lejos.robotics.SampleProvider;
 import lejos.hardware.motor.EV3LargeRegulatedMotor;
 import lejos.hardware.Sound;
 
 public class Navigation extends Thread{
 	
+  EV3LargeRegulatedMotor leftMotor;
+  EV3LargeRegulatedMotor rightMotor;
 	
-	EV3LargeRegulatedMotor leftMotor;
-	EV3LargeRegulatedMotor rightMotor;
+  double leftRadius;                    //Holds the left wheel radius (cm)
+  double rightRadius;                   //Holds the right wheel radius (cm)
+  double width;                         //Holds the length of the wheel base (cm)
 	
-	double leftRadius;
-	double rightRadius;
-	double width;
+  private static final int FORWARD_SPEED = 100;     //Default drive speed (deg/sec)
+  private static final int ROTATE_SPEED = 50;       //Default rotate speed (deg/sec)
+  private static final int MINIMUM_DISTANCE = 25;   //Minimum distance to activate avoidance (cm)
+  private boolean isNavigating = false;             //Checks if travelTo() or turnTo() is being called.
+  private double toTheta;               //Used to correct the robot's orientation (in degrees)
+  private Odometer odometer;
+  private boolean navigating = false;   //Initially, the robot is not navigating
+  public double x, y;                   //The desired x and y coordinates of the next checkpoint.
+  private double points[][] = {{30.48,30.48},{0,60.96},{60.96, 60.96},{60.96, 30.48},{30.48, 0.0}};;
 	
-	private static final int FORWARD_SPEED = 100;
-	private static final int ROTATE_SPEED = 50;
-	private static final int MINIMUM_DISTANCE = 25;
-	private static final double WHEEL_RADIUS = 2.1;
-	private double toTheta;
-	private Odometer odometer;
-	private boolean navigating = false;
-	public double x, y;
-	private double points[][] = {{30.48,30.48},{0,60.96},{60.96, 60.96},{60.96, 30.48},{30.48, 0.0}};;
+   public Navigation(Odometer odo, EV3LargeRegulatedMotor leftMotor, EV3LargeRegulatedMotor rightMotor, double leftRadius, double rightRadius, double width) {
+     //Here, objects instantiated in NavigationLab are passed to the Navigation Class.
+     this.odometer = odo;
+     this.leftMotor = leftMotor;
+     this.rightMotor = rightMotor;
+     this.leftRadius = leftRadius;
+     this.rightRadius = rightRadius;
+     this.width = width;
+	 }
 	
-	public Navigation(Odometer odo, EV3LargeRegulatedMotor leftMotor, EV3LargeRegulatedMotor rightMotor, double leftRadius, double rightRadius, double width) {
-	    this.odometer = odo;
-	    this.leftMotor = leftMotor;
-	    this.rightMotor = rightMotor;
-	    this.leftRadius = leftRadius;
-	    this.rightRadius = rightRadius;
-	    this.width = width;
-	  }
-	
-	public void run(){
-		
-		for (double[] p : points){
-			try {
-			      Thread.sleep(1000);
-			    } catch (InterruptedException e) {
-			      
-		    }
-			x = p[0];
-			y = p[1];
-			this.travelTo(p[0], p[1]);
-			
-		}
-		
-	leftMotor.stop();
+  public void run(){
+    for (double[] p : points){
+	  try {
+	    Thread.sleep(1000);        //Wait 1s before navigating (after button is pushed)
+	  } catch (InterruptedException e) { 		  
+	  }                        
+	  x = p[0];                    //Load the next checkpoint from
+	  y = p[1];                    //points into x and y
+	  this.isNavigating = true;    //The robot will now be navigating.
+	  this.travelTo(p[0], p[1]);   //Travels to the next point
+	  this.isNavigating = false;   //The robot has finished navigating
+      }
+    leftMotor.stop();              //Stops motors after end is reached.
     rightMotor.stop();
-	
-		
 	}
-	
-	
-	
-	
-	//public double points[][] = {{0.0,2.0},{1.0,1.0},{2.0,2.0},{2.0,1.0},{1.0,0.0}};
-	//public double Start[] = {0.0,0.0};
-	
-	public void travelTo(double x, double y) {
-		
-		toTheta = this.getAngle(x, y) - odometer.getTheta();
-		
-		if(toTheta > 180){
-			toTheta -= 360.0;
-		}else if(toTheta < -180){
-			toTheta += 360.0;
-		}
-		
-		if((this.odometer.getTheta() <= (this.getAngle(x, y) -1)) || (this.odometer.getTheta() >= (this.getAngle(x, y) + 1)) ){
 
-		turnTo(toTheta);
-		
-		}
-		
-			while(this.stop(x, y) == 0){
-			
-			if(UltrasonicPoller.getDistance() < MINIMUM_DISTANCE){
-				
-				//Method#1
-				/*
-				while(1==1){
-					
-					leftMotor.rotate(convertAngle(leftRadius, width, 90), true);
-				    rightMotor.rotate(-convertAngle(rightRadius, width, 90), false);
-				    leftMotor.setSpeed(FORWARD_SPEED);
-				    rightMotor.setSpeed(FORWARD_SPEED);
-				    leftMotor.forward();
-				    rightMotor.forward();
-				    leftMotor.rotate(convertDistance(leftRadius, 30), true);
-				    rightMotor.rotate(convertDistance(rightRadius, 30), false);
-				    leftMotor.rotate(-convertAngle(leftRadius, width, 90), true);
-				    rightMotor.rotate(convertAngle(rightRadius, width, 90), false);
-				    toTheta = this.getAngle(x, y) - odometer.getTheta();
-					
-					if(toTheta > 180){
-						toTheta -= 360.0;
-					}else if(toTheta < -180){
-						toTheta += 360.0;
-					}
-					
-					if((this.odometer.getTheta() <= (this.getAngle(x, y) -1)) || (this.odometer.getTheta() >= (this.getAngle(x, y) + 1)) ){
-
-					turnTo(toTheta);
-					
-					}
-				    
-
-					break;
-				}*/
-				double beforeTheta;
-				double afterTheta;
-				double deltaTheta;
-				double correction;
-				double distanceFromWall;
-				
-				distanceFromWall = UltrasonicPoller.getDistance();
-				
-				while (1==1){
-				
-				beforeTheta = odometer.getTheta();
-				
-				while(UltrasonicPoller.getDistance() < 40){
-					leftMotor.rotate(convertAngle(leftRadius, width, 5), true);
-				    rightMotor.rotate(-convertAngle(rightRadius, width, 5), false);
-				}
-				leftMotor.rotate(convertAngle(leftRadius, width, 10), true);
-			    rightMotor.rotate(-convertAngle(rightRadius, width, 10), false);
-			    
-			    afterTheta = odometer.getTheta();
-			    
-			    deltaTheta = Math.abs(beforeTheta-afterTheta);
-				
-			    correction = distanceFromWall/Math.cos(deltaTheta*((Math.PI)/(180)));
-			    
-			    leftMotor.rotate(convertDistance(leftRadius, correction), true);
-			    rightMotor.rotate(convertDistance(rightRadius, correction), false);
-			    
-			    leftMotor.rotate(convertDistance(leftRadius, 8), true);
-			    rightMotor.rotate(convertDistance(rightRadius, 8), false);
-			    
-			    
-			    leftMotor.rotate(-convertAngle(leftRadius, width, 180-2*deltaTheta), true);
-			    rightMotor.rotate(convertAngle(rightRadius, width, 180-2*deltaTheta), false);
-			    
-			    
-			    leftMotor.rotate(convertDistance(leftRadius, correction), true);
-			    rightMotor.rotate(convertDistance(rightRadius, correction), false);
-			    
-			    toTheta = this.getAngle(x, y) - odometer.getTheta();
-				
-				if(toTheta > 180){
-					toTheta -= 360.0;
-				}else if(toTheta < -180){
-					toTheta += 360.0;
-				}
-				
-				if((this.odometer.getTheta() <= (this.getAngle(x, y) -1)) || (this.odometer.getTheta() >= (this.getAngle(x, y) + 1)) ){
-
-				turnTo(toTheta);
-				
-				}
-				
-				break;
-				
-				
-				}
-				
-				
-				
+  public void travelTo(double x, double y) {
+    
+    //toTheta will store the angle the robot must orient itself to point in the direction
+    //of the next checkpoint
+    toTheta = this.getAngle(x, y) - odometer.getTheta();
+	  if(toTheta > 180){           //The correction angle must always be the acute angle.
+	    toTheta -= 360.0;          //If it is not, then it is turned into one.
+	  }else if(toTheta < -180){
+	    toTheta += 360.0;
+	  }
+      if((this.odometer.getTheta() <= (this.getAngle(x, y) -1)) 
+        || (this.odometer.getTheta() >= (this.getAngle(x, y) + 1)) ){
+      //checks to see if the robot is not facing the right way
+        turnTo(toTheta);            //Corrects the bot's orientation
+	  }
+	  while(this.stop(x, y) == 0){  //While the robot has not reached its next checkpoint.
+	    if(UltrasonicPoller.getDistance() < MINIMUM_DISTANCE){
+		  double beforeTheta;
+		  double afterTheta;
+		  double deltaTheta;
+		  double correction;
+		  double distanceFromWall;
+		  distanceFromWall = UltrasonicPoller.getDistance();
+		  while (1==1){
+		    beforeTheta = odometer.getTheta();
+		    while(UltrasonicPoller.getDistance() < 40){
+			  leftMotor.rotate(convertAngle(leftRadius, width, 5), true);
+			  rightMotor.rotate(-convertAngle(rightRadius, width, 5), false);
 			}
-		
+			leftMotor.rotate(convertAngle(leftRadius, width, 10), true);
+			rightMotor.rotate(-convertAngle(rightRadius, width, 10), false);
+			afterTheta = odometer.getTheta();
+            deltaTheta = Math.abs(beforeTheta-afterTheta);
+            correction = distanceFromWall/Math.cos(deltaTheta*((Math.PI)/(180)));
+            leftMotor.rotate(convertDistance(leftRadius, correction), true);
+            rightMotor.rotate(convertDistance(rightRadius, correction), false);
+            leftMotor.rotate(convertDistance(leftRadius, 8), true);
+            rightMotor.rotate(convertDistance(rightRadius, 8), false);
+            leftMotor.rotate(-convertAngle(leftRadius, width, 180-2*deltaTheta), true);
+            rightMotor.rotate(convertAngle(rightRadius, width, 180-2*deltaTheta), false);
+            leftMotor.rotate(convertDistance(leftRadius, correction), true);
+            rightMotor.rotate(convertDistance(rightRadius, correction), false);
+            toTheta = this.getAngle(x, y) - odometer.getTheta();
+            if(toTheta > 180){
+			  toTheta -= 360.0;
+			}else if(toTheta < -180){
+			  toTheta += 360.0;
+			}
+            if((this.odometer.getTheta() <= (this.getAngle(x, y) -1)) || (this.odometer.getTheta() >= (this.getAngle(x, y) + 1)) ){
+              turnTo(toTheta);
+            }
+            break;
+		  }	
+	    }
 		leftMotor.setSpeed(FORWARD_SPEED);
 	    rightMotor.setSpeed(FORWARD_SPEED);
 	    leftMotor.forward();
 	    rightMotor.forward();
-	    
-	    
-		}
-		 
-			
-			Sound.beep();      //Make a beeping sound
-		
+      }	
+	  Sound.beep();      //Make a beeping sound
 	}
 	
-	public void turnTo(double theta) {
-		
-	    this.navigating = true;
-		
-		leftMotor.setSpeed(ROTATE_SPEED);
-	    rightMotor.setSpeed(ROTATE_SPEED);
-
-	    leftMotor.rotate(convertAngle(leftRadius, width, theta), true);
-	    rightMotor.rotate(-convertAngle(rightRadius, width, theta), false);
-	      //leftMotor.rotate(convertAngle(leftRadius, width, theta), true);
-	      //rightMotor.rotate(-convertAngle(rightRadius, width, theta), false);
-	    
-	    //leftMotor.stop();
-	    //rightMotor.stop();
-	    
-	    this.navigating = false;
-	    
-	    /*try {
-		      Thread.sleep(500);
-		    } catch (InterruptedException e) {
-		      
-		    }*/
-	      
-		
-	}
+    public void turnTo(double theta) {
+	  this.navigating = true;              //Sets the robot to navigation mode
+      leftMotor.setSpeed(ROTATE_SPEED);    //Sets the motors to rotation speed  
+      rightMotor.setSpeed(ROTATE_SPEED);
+      
+      //Turns both motors by the correction angle of theta (in degrees).
+      leftMotor.rotate(convertAngle(leftRadius, width, theta), true);
+      rightMotor.rotate(-convertAngle(rightRadius, width, theta), false);
+      this.navigating = false;              //Turns navigation mode off
+    }
 	
 	private double getAngle(double x, double y){
-		
-		double theta;
-		double deltaX = x - this.odometer.getX();
-		double deltaY = y - this.odometer.getY();
-		
-		
-		
-		theta = 90-(Math.atan(deltaY/deltaX)*(180/Math.PI));
-		
-		if(deltaX < 0) {
-			theta += 180;
-		}
-		
-		//System.out.println(theta);
-		
-		return theta;
+      double theta;
+      //Calculates the angle the robot must orient itself in to face the checkpoint.
+      //This is done using the robot's current position, the checkpoint position,
+      //and simple trigonometry.
+	  double deltaX = x - this.odometer.getX();
+	  double deltaY = y - this.odometer.getY();
+	  theta = 90-(Math.atan(deltaY/deltaX)*(180/Math.PI)); //The angle must be converted first.
+	  if(deltaX < 0) {
+	    theta += 180;
+	  }
+    return theta;
 	}
 	
 	private int stop(double x, double y) {
-	    
-		int stop = 1;  //true
-		
-		
-		if(this.odometer.getX() < x - 2 || this.odometer.getX() > x + 2){
-			stop = 0;
-		}
-		
-		if(this.odometer.getY() < y - 2 || this.odometer.getY() > y + 2){
-			stop = 0;
-		}
-		
-		
-		return stop;
-		
+      int stop = 1;  //true
+      if(this.odometer.getX() < x - 2 || this.odometer.getX() > x + 2){
+      stop = 0;
+      }
+      if(this.odometer.getY() < y - 2 || this.odometer.getY() > y + 2){
+        stop = 0;
+      }
+      return stop;
 	  }
 	
 	private static int convertAngle(double radius, double width, double angle) {
-	    return convertDistance(radius, Math.PI * width * angle / 360.0);
-	  }
-	private static int convertDistance(double radius, double distance) {
-	    return (int) ((180.0 * distance) / (Math.PI * radius));
-	  }
-	
-	
-	public static void drive(EV3LargeRegulatedMotor leftMotor, EV3LargeRegulatedMotor rightMotor, double leftRadius, double rightRadius, double width){
-	
-	
+	  return convertDistance(radius, Math.PI * width * angle / 360.0);
 	}
-	
-	/*private boolean isNavigating() {
-		
-	}*/
-	
+	private static int convertDistance(double radius, double distance) {
+	  return (int) ((180.0 * distance) / (Math.PI * radius));
+	}
+
+	private boolean isNavigating() {
+	  return this.isNavigating;
+	}
 }
--- src/ca/mcgill/ecse211/navigationlab/NavigationLab.java
+++ src/ca/mcgill/ecse211/navigationlab/NavigationLab.java
@@ -1,97 +1,70 @@
 package ca.mcgill.ecse211.navigationlab;
 
 import lejos.hardware.Button;
-	import lejos.hardware.ev3.LocalEV3;
-	import lejos.hardware.lcd.TextLCD;
-	import lejos.hardware.motor.EV3LargeRegulatedMotor;
-	import lejos.hardware.port.MotorPort;
-	import lejos.hardware.port.Port;
-	import lejos.hardware.sensor.SensorModes;
-	import lejos.hardware.sensor.EV3ColorSensor;
-	import lejos.hardware.sensor.EV3UltrasonicSensor;
-	import lejos.robotics.SampleProvider;
-	import lejos.utility.Delay;
+import lejos.hardware.ev3.LocalEV3;
+import lejos.hardware.lcd.TextLCD;
+import lejos.hardware.motor.EV3LargeRegulatedMotor;
+import lejos.hardware.port.Port;
+import lejos.hardware.sensor.SensorModes;
+import lejos.hardware.sensor.EV3UltrasonicSensor;
+import lejos.robotics.SampleProvider;
 
 	
 public class NavigationLab {
-	
-	public double points[][] = {{0.0,2.0},{1.0,1.0},{2.0,2.0},{2.0,1.0},{1.0,0.0}};
-	public double Start[] = {0.0,0.0};
 
-	  private static final EV3LargeRegulatedMotor leftMotor =
-	      new EV3LargeRegulatedMotor(LocalEV3.get().getPort("A"));
-	  
-	  private static final EV3LargeRegulatedMotor rightMotor =
-	      new EV3LargeRegulatedMotor(LocalEV3.get().getPort("D"));
-	  
-	  private static final Port csPort = LocalEV3.get().getPort("S1");
-	  private static final Port usPort = LocalEV3.get().getPort("S2");
-
-	  public static final double WHEEL_RADIUS = 2.1;
-	  public static final double TRACK = 15.0;
-	  
-	  public static void main(String[] args) {
+  public static final double WHEEL_RADIUS = 2.1;            //Wheel radius (cm)
+  public static final double TRACK = 15.0;                  //Wheel base length (cm)
+  
+  //Setup right and left motors
+  private static final EV3LargeRegulatedMotor leftMotor =   //Left motor uses port A
+    new EV3LargeRegulatedMotor(LocalEV3.get().getPort("A"));
+  private static final EV3LargeRegulatedMotor rightMotor =  //Right motor uses port D
+    new EV3LargeRegulatedMotor(LocalEV3.get().getPort("D"));
+  
+  //Initialize ultrasonic sensor to port 2
+  private static final Port usPort = LocalEV3.get().getPort("S2");  
+  
+  public static void main(String[] args) {
+    int buttonChoice = 0;                                   //Contains the user input
+    final TextLCD t = LocalEV3.get().getTextLCD();          //Initialize text field          
+    Odometer odometer = new Odometer(leftMotor, rightMotor);//Initialize odometer
+    OdometryDisplay odometryDisplay = new OdometryDisplay(odometer, t); //Initialize Odometer display
+		                            
+    //Create an instance of the US sensor, and a buffer to contain its data
+    @SuppressWarnings("resource")
+    SensorModes usSensor = new EV3UltrasonicSensor(usPort);
+    SampleProvider usDistance = usSensor.getMode("Distance"); 
+    float[] usData = new float[usDistance.sampleSize()];
+    
+	//Create an instance of the US Poller, to take samples from the US sensor in a thread.	  
+    UltrasonicPoller usPoller = new UltrasonicPoller(usDistance,usData);
+    usPoller.start();
 		  
-		  int buttonChoice = 0;
-		  
-		  /*@SuppressWarnings("resource")
-		  EV3ColorSensor csSensor = new EV3ColorSensor(csPort);
-		  SampleProvider csColor = csSensor.getMode("Red");
-		  float[] csData = new float[csColor.sampleSize()];
-		  
-		  ColorSensorPoller csPoller = new ColorSensorPoller(csColor,csData);
-		  csPoller.start();*/
-		  
-		  final TextLCD t = LocalEV3.get().getTextLCD();
-		  Odometer odometer = new Odometer(leftMotor, rightMotor);
-		  OdometryDisplay odometryDisplay = new OdometryDisplay(odometer, t);
-		  
-		  
-		  @SuppressWarnings("resource")
-		  SensorModes usSensor = new EV3UltrasonicSensor(usPort);
-		  SampleProvider usDistance = usSensor.getMode("Distance"); 
-		  float[] usData = new float[usDistance.sampleSize()];
-		  
-		  UltrasonicPoller usPoller = new UltrasonicPoller(usDistance,usData);
-		  usPoller.start();
-		  
-		  final Navigation navigator = new Navigation(odometer, leftMotor, rightMotor,WHEEL_RADIUS, WHEEL_RADIUS, TRACK );
-		  Avoid avoider = new Avoid(odometer, navigator, leftMotor, rightMotor);
-		  
-		  (new Thread() {
-		        public void run() {
-		          //Navigation.drive(leftMotor, rightMotor, WHEEL_RADIUS, WHEEL_RADIUS, TRACK);
-		        }
-		      }).start();
-		  
-
-		  do {
-		      // clear the display
-		      t.clear();
-
-		      
-		      t.drawString("                ", 0, 0);
-		      t.drawString("  PRESS ENTER   ", 0, 1);
-		      t.drawString("   TO BEGIN     ", 0, 2);
-		      t.drawString("                ", 0, 3);
-		      t.drawString("                ", 0, 4);
-
-		      buttonChoice = Button.waitForAnyPress();
-		    } while (buttonChoice != Button.ID_ENTER);
-
-		  
-	      if(buttonChoice == Button.ID_ENTER){
-	          avoider.start();
-	    	  odometer.start();
-	          odometryDisplay.start();
-	          navigator.start();
-	    	  
-	      }
-	      
-	      
-	      
-		  while (Button.waitForAnyPress() != Button.ID_ESCAPE);
-		  System.exit(0);
-		  
+    //Create an instance of the Nagivation class, used to guide the robot.
+    final Navigation navigator = 
+    new Navigation(odometer, leftMotor, rightMotor,WHEEL_RADIUS, WHEEL_RADIUS, TRACK );
+    (new Thread() {
+      public void run() {  
 	  }
+    }).start();
+    do { 
+      t.clear();	                           // Clear the display.
+      t.drawString("                ", 0, 0);  // Prompt user for input
+      t.drawString("  PRESS ENTER   ", 0, 1);
+      t.drawString("   TO BEGIN     ", 0, 2);
+      t.drawString("                ", 0, 3);
+      t.drawString("                ", 0, 4);
+      
+      buttonChoice = Button.waitForAnyPress(); //Wait for user input
+	} while (buttonChoice != Button.ID_ENTER); //Wait for the user to press enter.
+      if(buttonChoice == Button.ID_ENTER){
+        //Pressing Enter will cause the main operating threads to begin.
+	    odometer.start();                  
+	    odometryDisplay.start();
+	    navigator.start();
+	    } 
+        while (Button.waitForAnyPress() != Button.ID_ESCAPE);
+        //The Program waits here, and exits if the user presses the escape button.
+        System.exit(0);  
+  }   
 }
