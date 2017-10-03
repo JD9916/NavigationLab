fc32db94dfae2f6de37ed795534fc8a96dcbf64c
jeremy-davis-mcgill <jeremy.davis@mail.mcgill.ca>
2017-10-03 12:21:55 -0400

    More comments, no more avoid class file

--- src/ca/mcgill/ecse211/navigationlab/Navigation.java
+++ src/ca/mcgill/ecse211/navigationlab/Navigation.java
@@ -65,31 +65,50 @@ public class Navigation extends Thread{
 	  }
 	  while(this.stop(x, y) == 0){  //While the robot has not reached its next checkpoint.
 	    if(UltrasonicPoller.getDistance() < MINIMUM_DISTANCE){
-		  double beforeTheta;
-		  double afterTheta;
-		  double deltaTheta;
-		  double correction;
-		  double distanceFromWall;
+		  double beforeTheta;     //The robots orientation before avoidance
+		  double afterTheta;      //The robots orientation after facing away from the wall
+		  double deltaTheta;      //The diff between beforeTheta and afterTheta
+		  double correction;      //How far the robot must drive to avoid the obstacle
+		  double distanceFromWall;//How far the robot is from the wall
 		  distanceFromWall = UltrasonicPoller.getDistance();
+		  
+		  //This avoidance algorithm works by
+		  //1. Turning until it sees no object
+		  //2. Drive forward until past the object
+		  //3. Turn back the necessary angle to come behind the object
+		  //4. Drive back behind the object
+		  
+		  
 		  while (1==1){
-		    beforeTheta = odometer.getTheta();
+		    beforeTheta = odometer.getTheta();     //Get the robot's initial orientation
 		    while(UltrasonicPoller.getDistance() < 40){
+		      //Here, the robot will turn until it sees no wall
 			  leftMotor.rotate(convertAngle(leftRadius, width, 5), true);
 			  rightMotor.rotate(-convertAngle(rightRadius, width, 5), false);
 			}
+		    //Once the robot sees no wall, it turns an extra 10deg to make sure it will
+		    //not come close to the wall when driving straight
 			leftMotor.rotate(convertAngle(leftRadius, width, 10), true);
 			rightMotor.rotate(-convertAngle(rightRadius, width, 10), false);
-			afterTheta = odometer.getTheta();
-            deltaTheta = Math.abs(beforeTheta-afterTheta);
+			
+			afterTheta = odometer.getTheta();     //Get the robot's new orientation
+            deltaTheta = Math.abs(beforeTheta-afterTheta);  //Compute deltaTheta
+            //Compute how far the robot must travel to drive past the wall
             correction = distanceFromWall/Math.cos(deltaTheta*((Math.PI)/(180)));
+            //Drive past the wall
             leftMotor.rotate(convertDistance(leftRadius, correction), true);
             rightMotor.rotate(convertDistance(rightRadius, correction), false);
+            //The two lines below tell the robot to drive an extra distance forward to
+            //composate for the width of the walls
             leftMotor.rotate(convertDistance(leftRadius, 8), true);
             rightMotor.rotate(convertDistance(rightRadius, 8), false);
+            //The robot turns the necessary angle to point itself behind the wall
             leftMotor.rotate(-convertAngle(leftRadius, width, 180-2*deltaTheta), true);
             rightMotor.rotate(convertAngle(rightRadius, width, 180-2*deltaTheta), false);
+            //The robot drives straight to behind the wall, and it is now past it.
             leftMotor.rotate(convertDistance(leftRadius, correction), true);
             rightMotor.rotate(convertDistance(rightRadius, correction), false);
+            //Now the robot must orient itself towards the coordinates again.
             toTheta = this.getAngle(x, y) - odometer.getTheta();
             if(toTheta > 180){
 			  toTheta -= 360.0;
@@ -102,6 +121,7 @@ public class Navigation extends Thread{
             break;
 		  }	
 	    }
+	    //If facing the right angle, and no object is detected, move forward.
 		leftMotor.setSpeed(FORWARD_SPEED);
 	    rightMotor.setSpeed(FORWARD_SPEED);
 	    leftMotor.forward();
@@ -128,32 +148,40 @@ public class Navigation extends Thread{
       //and simple trigonometry.
 	  double deltaX = x - this.odometer.getX();
 	  double deltaY = y - this.odometer.getY();
-	  theta = 90-(Math.atan(deltaY/deltaX)*(180/Math.PI)); //The angle must be converted first.
+	  //note: The angle must be converted from radians to degrees.
+	  theta = 90-(Math.atan(deltaY/deltaX)*(180/Math.PI)); 
 	  if(deltaX < 0) {
-	    theta += 180;
+	    theta += 180;  //This correction is included to fix negative angles.
 	  }
     return theta;
 	}
 	
 	private int stop(double x, double y) {
-      int stop = 1;  //true
+	  //This method is used to check if the robot has reached it's next
+	  //coordinates yet. Naturally, there will need to be some room of error.
+      int stop = 1;  //Initially set stop to true.
       if(this.odometer.getX() < x - 2 || this.odometer.getX() > x + 2){
-      stop = 0;
+      stop = 0;     //If the X coordinate is not correct, set stop to false.
       }
       if(this.odometer.getY() < y - 2 || this.odometer.getY() > y + 2){
-        stop = 0;
+        stop = 0;   //If the Y coordinate is not correct, set stop to false.
       }
       return stop;
 	  }
 	
 	private static int convertAngle(double radius, double width, double angle) {
+	  //Given wheel radius (cm), wheel base (cm), and desired turn angle (degrees)
+	  //converts to the angle the wheels need to drive.
 	  return convertDistance(radius, Math.PI * width * angle / 360.0);
 	}
 	private static int convertDistance(double radius, double distance) {
+	  //Given the distance (cm) to travel and the wheel radius (cm), 
+	  //converts to amount of wheel rotation needed.
 	  return (int) ((180.0 * distance) / (Math.PI * radius));
 	}
 
 	private boolean isNavigating() {
+	  //Returns true if travelTo() or turnTo() have been called and not yet returned
 	  return this.isNavigating;
 	}
 }
